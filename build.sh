4c907563bb841822117160d4a835c335cdad5227
Albin Biju <albinbiju0330@gmail.com>
2026-08-29 15:06:33 +0530

    Force install Pillow directly via pip

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 python -m pip install --upgrade pip
 python -m pip install pipenv
 
-python -m pipenv install
+python -m pipenv install --system
+
+# Force install Pillow to bypass Pipenv lock issues
+python -m pip install Pillow
+
 python manage.py collectstatic --no-input
 python manage.py migrate
